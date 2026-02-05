506c94ffa887353003eab8a068e153d50fdde7ba
Saivaibhav <kssaivaibhav@brevo.com>
2026-02-05 12:34:20 +0530

    feat: float decimals

--- app.go
+++ app.go
@@ -3,9 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var a int
-	var b int
-	fmt.Print("Enter 2 variables")
-	fmt.Scan(&a, &b)
-	fmt.Printf("The var are %d and %d\n", a, b)
+	var principal float64
+	var rate float64
+	var time float64
+
+	fmt.Println("Enter Principal amount:")
+	fmt.Scan(&principal)
+	fmt.Println("Enter Rate of Interest:")
+	fmt.Scan(&rate)
+	fmt.Println("Enter Time in years:")
+	fmt.Scan(&time)
+
+	si := (principal * rate * time) / 100
+
+	fmt.Printf("Simple Interests is : %f\n", si)
 }
