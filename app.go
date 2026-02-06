d12209891505a6abe2d0a50137c6f2b88a7ec7c2
Saivaibhav <kssaivaibhav@brevo.com>
2026-02-06 18:03:01 +0530

    feat: array

--- app.go
+++ app.go
@@ -2,49 +2,12 @@ package main
 
 import "fmt"
 
-type employee struct {
-	name   string
-	age    int
-	salary float64
-}
-
-// reciever type
-func (emp *employee) output() {
-	fmt.Println("employee name:", emp.name)
-	fmt.Println("employee age:", emp.age)
-	fmt.Println("employee salary:", emp.salary)
-}
-
-func (emp *employee) input() {
-	fmt.Println("Enter employee details: ")
-	fmt.Print("Name: ")
-	fmt.Scanln(&emp.name)
-
-	fmt.Print("Age: ")
-	fmt.Scanln(&emp.age)
-
-	fmt.Print("Salary: ")
-	fmt.Scanln(&emp.salary)
-}
-
 func main() {
-	// emp1 := employee{
-	// 	name:   "Vaibhav",
-	// 	age:    22,
-	// 	salary: 50000.0,
-	// }
+	arr := []int{1, 2, 3, 4, 5}
 
-	var emp1 employee
+	var temp [5]int
 
-	emp1.input()
-	fmt.Println("employee details:", emp1)
+	fmt.Println("Enter the size of the array: ", arr)
+	fmt.Println("Enter the size of the array: ", temp)
 
-	// showDetails(&emp1)
-	emp1.output()
-}
-
-func showDetails(emp *employee) {
-	fmt.Println("employee name:", emp.name)
-	fmt.Println("employee age:", emp.age)
-	fmt.Println("employee salary:", emp.salary)
 }
