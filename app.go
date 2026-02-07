f0a016a533e88997e6533f47012ef3cf41981d90
Saivaibhav <kssaivaibhav@brevo.com>
2026-02-07 11:33:50 +0530

    feat: slice

--- app.go
+++ app.go
@@ -4,7 +4,10 @@ func main() {
 
 	arr := []int{1, 2, 34, 56}
 
-	for i, v := range arr {
-		println("Index:", i, "Value:", v)
-	}
+	slice1 := arr[1:3]
+
+	slice1[0] = 45
+
+	println("Array:", arr[0], arr[1], arr[2], arr[3])
+
 }
